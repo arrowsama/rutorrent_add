7c06d84a69eab255b0c280d535eaf3be8ed56036
Wendor <weny@bk.ru>
2015-04-28 23:15:44 +0600

    default name for generated plugin

--- make_xpi.sh
+++ make_xpi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-last_commit=$(git rev-parse --verify HEAD | cut -c1-7)
+#last_commit=$(git rev-parse --verify HEAD | cut -c1-7)
+#zip -r xpi/rutorrent_add-0.1.0-$last_commit-fx.xpi content defaults locale modules skin chrome.manifest install.rdf
 
-zip -r xpi/rutorrent_add-0.0.15-$last_commit-fx.xpi content defaults locale modules skin chrome.manifest install.rdf
+zip -r xpi/rutorrent_add-fx-last.xpi content defaults locale modules skin chrome.manifest install.rdf
