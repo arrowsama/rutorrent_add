0ae47a0764e920432b4d16cd15e03e208a4ef7fa
Wendor <weny@bk.ru>
2015-04-28 17:21:25 +0600

    add xpi folder

--- make_xpi.sh
+++ make_xpi.sh
@@ -2,4 +2,4 @@
 
 last_commit=$(git rev-parse --verify HEAD | cut -c1-7)
 
-zip -r rutorrent_add-0.0.15-$last_commit-fx.xpi content defaults locale modules skin chrome.manifest install.rdf
+zip -r xpi/rutorrent_add-0.0.15-$last_commit-fx.xpi content defaults locale modules skin chrome.manifest install.rdf
