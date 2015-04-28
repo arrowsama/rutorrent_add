27874bb4f6ae47cb062c8c6196857c1ebe914826
Wendor <weny@bk.ru>
2015-04-29 01:06:07 +0600

    add pre-commit hook

--- make_xpi.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r xpi/rutorrent_add-fx-last.xpi content defaults locale modules skin chrome.manifest install.rdf
---
+++ pre-commit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/bin/zip -r xpi/rutorrent_add-fx-last.xpi content defaults locale modules skin chrome.manifest install.rdf
